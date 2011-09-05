c9f0faec160d8f3840a1127775113b0b1ff1dc65
Jeffrey Johnson <ortelius@gmail.com>
2011-09-05 15:04:33 -0700

    Removing some old? (or at least unused templates)

--- clio/common/views.py
+++ clio/common/views.py
@@ -83,7 +83,6 @@ def index(request):
     return render_to_response('index.html',
         {
         },context_instance=RequestContext(request))
-  #return render_to_response("mainindex.html")
 
 
 def exportcsv(request):
